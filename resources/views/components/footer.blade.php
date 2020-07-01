d190af671cf3b011d1ceaf73b283801f6a47fd8d
kaito <kaitooshiro1@icloud.com>
2020-07-01 17:38:29 +0900

    管理者画面へのリンク

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,6 @@
 @section('footer')
+    <div style="text-align: center;">
+        <a href="/admin">管理者画面へ</a>
+    </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 @endsection
